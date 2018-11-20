1232b6b27ef901ae2b07035e4303b1902153a58a
שבתי גולדברג <shgoldbe@g.jct.ac.il>
2018-11-20 14:37:41 +0200

    my second commit

--- BE/Address.cs
+++ BE/Address.cs
@@ -4,6 +4,7 @@ using System.Text;
 
 namespace BE
 {
+
 	public struct Address
 	{
 		string city, street, apptNumber;
--- BE/Trainee.cs
+++ BE/Trainee.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace BE
-{
+{  
 	class Trainee:Person
 	{
 		private GearType gearType;
