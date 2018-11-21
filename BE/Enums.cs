cb8581cf62cd7026770b2cff1629173b747428c3
שבתי גולדברג <shgoldbe@g.jct.ac.il>
2018-11-21 18:07:24 +0200

    gdfd

--- BE/Enums.cs
+++ BE/Enums.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace BE
-{ 
+{  
+    //jbvhcgfx
 	public enum Gender { male, female };
 	public enum CarType { motorCycle, privateCar, smallTruck, largeTruck };
 	public enum GearType { automatic, Manual };
