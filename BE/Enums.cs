c58b4f7ae5c16a7252133d6ce9c956c5c75e3b3c
שבתי גולדברג <shgoldbe@g.jct.ac.il>
2018-11-21 10:55:31 +0200

    commit

--- BE/Enums.cs
+++ BE/Enums.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace BE
-{
+{ 
 	public enum Gender { male, female };
 	public enum CarType { motorCycle, privateCar, smallTruck, largeTruck };
 	public enum GearType { automatic, Manual };
