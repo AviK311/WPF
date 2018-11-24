226ba442dca7d327d4d202a52942608cf0c3ea56
שבתי גולדברג <shgoldbe@g.jct.ac.il>
2018-11-24 19:20:46 +0200

    delete garbage

--- BE/Enums.cs
+++ BE/Enums.cs
@@ -4,7 +4,6 @@ using System.Text;
 
 namespace BE
 {  
-    //jbvhcgfx
 	public enum Gender { male, female };
 	public enum CarType { motorCycle, privateCar, smallTruck, largeTruck };
 	public enum GearType { automatic, Manual };
