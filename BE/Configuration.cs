9ca8dca5154c002a97f1fcbd4f5271d1e84325c4
שבתי גולדברג <shgoldbe@g.jct.ac.il>
2018-11-25 12:04:57 +0200

    adding testCode

--- BE/Configuration.cs
+++ BE/Configuration.cs
@@ -6,6 +6,8 @@ namespace BE
 {
     class Configuration
     {
+        protected static int testCode = 0;
+        public int TestCode { get => testCode; set => testCode = value; }
         public static uint TimeBetweenTests { get; } = 7;
         public static uint MinAgaOfTrainee { get; } = 18;
         public static uint MinNumOfClasses { get; } = 20;
--- BL/IBL.cs
+++ BL/IBL.cs
@@ -8,6 +8,20 @@ namespace BL
 {
     public interface IBL
     {
+        void AddTest(BE.Test test);
+        void RemoveTest(string id);
+        void UpdateTest(string id, BE.Test newData);
 
+        void AddTrainee(BE.Trainee trainee);
+        void RemoveTrainee(string id);
+        void UpdateTrainee(string id, BE.Trainee newData);
+
+        void AddTester(BE.Tester tester);
+        void RemoveTester(string id);
+        void UpdateTester(string id, BE.Tester newData);
+
+        IEnumerable<BE.Test> GetTests();
+        IEnumerable<BE.Tester> GetTesters();
+        IEnumerable<BE.Trainee> GetTrainees();
     }
 }
