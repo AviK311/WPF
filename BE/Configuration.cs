f83930ca5b99e11cea2e9a5407301d36eb79c298
שבתי גולדברג <shgoldbe@g.jct.ac.il>
2018-11-25 11:26:46 +0200

    adding class BE.Configuration

---
+++ BE/Configuration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BE
+{
+    class Configuration
+    {
+        public static uint TimeBetweenTests { get; } = 7;
+        public static uint MinAgaOfTrainee { get; } = 18;
+        public static uint MinNumOfClasses { get; } = 20;
+        public static uint MinAgaOfTester { get; } = 40;
+    }
+}
