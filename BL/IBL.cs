541dc11574f9c99982d113cadfd0690e3558ee1c
שבתי גולדברג <shgoldbe@g.jct.ac.il>
2018-11-25 13:44:38 +0200

    Creating an interface for BL

--- BL/IBL.cs
+++ BL/IBL.cs
@@ -23,5 +23,22 @@ namespace BL
         IEnumerable<BE.Test> GetTests();
         IEnumerable<BE.Tester> GetTesters();
         IEnumerable<BE.Trainee> GetTrainees();
+
+        List<BE.Tester> TestersInRange(BE.Address address);//with GoogleMaps
+        List<BE.Tester> AvailableTesters(DateTime date);
+        List<BE.Test> AppropriateTests(Func<BE.Test, bool> match);
+        int TestsNum(BE.Trainee trainee);
+        bool ProperToLicense(BE.Trainee trainee);
+        IEnumerable<DateTime> PlannedTests();
+
+        //Groups
+        IEnumerable<IGrouping<string, BE.Trainee>> 
+            TraineesGroupsAccordingSchoolName(bool inOrder);
+        IEnumerable<IGrouping<BE.Name, BE.Trainee>>
+            TraineesGroupsAccordingTeacherName(bool inOrder);
+        IEnumerable<IGrouping<int, BE.Trainee>>
+            TraineesGroupsAccordingTestsNum(bool inOrder);
+        IEnumerable<IGrouping<BE.CarType, BE.Tester>>
+            TesterGroupsAccordingCarType(bool inOrder);
     }
 }
