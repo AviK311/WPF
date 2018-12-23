53deeb5be43cf0e49d3fcf03a8a73b07b5c7a8b4
Avi Koenigsberg <abkoen@gmail.com>
2018-12-23 23:14:09 +0200

    addTester

---
+++ UI-WPF/TesterAdd.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using BE;
+using BL;
+
+namespace UI_WPF
+{
+	/// <summary>
+	/// Interaction logic for TesterAdd.xaml
+	/// </summary>
+	public partial class TesterAdd : Window
+	{
+		Tester tester;
+		IBL bl;
+		public TesterAdd()
+		{
+			
+			InitializeComponent();
+			tester = new Tester();
+			DataContext = tester;
+
+			sexComboBox.ItemsSource = Enum.GetValues(typeof(Gender));
+			testingCarTypeComboBox.ItemsSource = Enum.GetValues(typeof(VehicleType));
+			
+		}
+		
+		
+	}
+}
