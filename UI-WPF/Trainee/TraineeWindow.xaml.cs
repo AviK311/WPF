1db6678de71a3f19465fd48dc364716b0914131f
תמר גולדשמיד <tgoldshm@g.jct.ac.il>
2019-01-26 23:32:33 +0200

    33

--- UI-WPF/Trainee/TraineeWindow.xaml.cs
+++ UI-WPF/Trainee/TraineeWindow.xaml.cs
@@ -111,16 +111,29 @@ namespace UI_WPF
         }
         private void image_MouseEnter(object sender, MouseEventArgs e)
         {
-            image.Visibility = Visibility.Collapsed;
             System.Threading.Thread.Sleep(200);
+            image.Visibility = Visibility.Collapsed;         
             image2.Visibility = Visibility.Visible;
         }
 
         private void image2_MouseEnter(object sender, MouseEventArgs e)
         {
-            image2.Visibility = Visibility.Collapsed;
             System.Threading.Thread.Sleep(200);
+            image2.Visibility = Visibility.Collapsed;          
+            image3.Visibility = Visibility.Visible;
+        }
+        private void image3_MouseEnter(object sender, MouseEventArgs e)
+        {
+            System.Threading.Thread.Sleep(200);
+            image3.Visibility = Visibility.Collapsed;
+            image4.Visibility = Visibility.Visible;
+        }
+        private void image4_MouseEnter(object sender, MouseEventArgs e)
+        {
+            System.Threading.Thread.Sleep(200);
+            image4.Visibility = Visibility.Collapsed;
             image.Visibility = Visibility.Visible;
         }
+
     }
 }
